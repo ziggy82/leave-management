97c700567b83b0dde1b01b94cf1a5141316bf1bb
Razak, Zaheir <zrazak3@gatech.edu>
2021-10-20 17:50:55 -0400

    Added some leave management view models and incorporated automapper to the map the data classes

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using leave_management.Contracts;
 using leave_management.Data;
+using leave_management.Mappings;
 using leave_management.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -37,7 +39,7 @@ namespace leave_management
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
             services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
-
+            services.AddAutoMapper(typeof(Maps));
 
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)

c0b93c857d28a140169c0fa9e59481a2462a8cee
Razak, Zaheir <zrazak3@gatech.edu>
2021-11-08 17:04:51 -0500

    added functionality and seeding of role into the system

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -43,13 +43,20 @@ namespace leave_management
 
 
             services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+        )
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +76,9 @@ namespace leave_management
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            //call static class
+            SeedData.Seed(userManager, roleManager);
 
             app.UseEndpoints(endpoints =>
             {
